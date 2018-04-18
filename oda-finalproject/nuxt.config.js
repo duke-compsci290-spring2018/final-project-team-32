a93e60894738addf09488dea395752f6a75f5236
SIERRA LORENZINI <sjl40@duke.edu>
2018-04-18 13:46:26 -0400

    link test

--- oda-finalproject/nuxt.config.js
+++ oda-finalproject/nuxt.config.js
@@ -2,13 +2,13 @@
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/final-project-team-32/oda-finalproject/'
+    base: '/final-project-team-32/oda-finalproject/dist/'
   }
 } : {}
 
 module.exports = {
   router: {
-    base: '/final-project-team-32/oda-finalproject/'
+    base: '/final-project-team-32/oda-finalproject/dist/'
   },
   /*
   ** Headers of the page
