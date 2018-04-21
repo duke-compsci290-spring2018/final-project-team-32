2cfba7c1cc95086c7e943c4296c5f35f44ed2a37
SIERRA LORENZINI <sjl40@duke.edu>
2018-04-21 14:51:16 -0400

    restructured links

---
+++ oda-finalproject/nuxt.config.js
@@ -0,0 +1,45 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/final-project-team-32/oda-finalproject/'
+  }
+} : {}
+module.exports = {
+  routerBase,
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'oda-finalproject',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    publicPath: '/assets/',
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
