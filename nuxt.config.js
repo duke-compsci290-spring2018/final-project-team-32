6356ba2aa8fd38e91c0a6f432e0582c4bb030d31
SIERRA LORENZINI <sjl40@duke.edu>
2018-04-21 12:50:47 -0400

    move nuxt to main repository

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/final-project-team-32/oda-finalproject/dist/'
+  }
+} : {}
+
+module.exports = {
+  // routerBase,
+  router: {
+    base: '/final-project-team-32/oda-finalproject/'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'oda-finalproject',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+
+  build: {
+    // publicPath: '/pages/',
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
