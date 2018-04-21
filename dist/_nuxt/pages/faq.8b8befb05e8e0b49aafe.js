94a83554c218d94c59945403386dd2c40511dc48
SIERRA LORENZINI <sjl40@duke.edu>
2018-04-21 14:36:33 -0400

    clear repository

--- dist/_nuxt/pages/faq.8b8befb05e8e0b49aafe.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([6],{NAEW:function(e,l,u){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);n.options.__file="pages/faq.vue",l.default=n.exports}});
--- dist/_nuxt/pages/why.ea11fa536fde6504af23.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([4],{"/G1K":function(e,l,u){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);n.options.__file="pages/why.vue",l.default=n.exports}});
