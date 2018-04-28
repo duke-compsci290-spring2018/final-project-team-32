171c4aec33e91c6bfb25fe79f1359e52d2deae9e
Yuuta Kendall <yrk3@duke.edu>
2018-04-27 23:20:28 -0400

    footer!

---
+++ oda-finalproject/components/SiteFooter.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="nav" class="nav">
+    <v-footer color="indigo" app inset>
+      <span class="white--text">&copy; Sierra, Rikki, Charlotte 2017</span>
+    </v-footer>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'site-footer',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
