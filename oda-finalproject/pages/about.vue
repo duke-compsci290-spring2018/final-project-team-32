ce1e8af83e64c09f7cabc957248e78c20af02e96
Charlotte Roh <hr67@duke.edu>
2018-04-28 15:28:40 -0400

    Add files via upload

--- oda-finalproject/pages/about.vue
+++ oda-finalproject/pages/about.vue
@@ -19,15 +19,15 @@
                   </p>
               </v-card-text>
               <v-card-text>
-                  Otros Dreams en Acción is significant in three important ways. First, ODA advocates for the claim to legal rights and responsibilities—including but not limited to legal international mobility—as members of the United States national community from our current location, al otro lado, in towns and cities across Mexico. Second, ODA advocates for integration and transformation in Mexico. And last but not least, the members of ODA support each other, independent of what someone may have done in the past, the circumstances under which someone returned to Mexico, and the diversity of immigration status and mobility within our community. 
+                  Otros Dreams en Acción is significant in three important ways. First, ODA advocates for the claim to legal rights and responsibilities—including but not limited to legal international mobility—as members of the United States national community from our current location, al otro lado, in towns and cities across Mexico. Second, ODA advocates for integration and transformation in Mexico. And last but not least, the members of ODA support each other, independent of what someone may have done in the past, the circumstances under which someone returned to Mexico, and the diversity of immigration status and mobility within our community.
               </v-card-text>
         </v-card>
         <v-card>
           <v-card-title>     <h1 class="sec02">Our Values</h1>
           </v-card-title>
           <v-card-text>
-            In ODA, we seek to be authentic and adventurous.  We are determined to contribute to our community by accepting challenges, as well as letting friendship guide us. We fight the good fight when it comes to Justice for everyone. Kindness, Accountability, Optimism, Community and Love are the values that we share and try to put into action every day.  
-            We know, firsthand, about how the current immigration system in the United States criminalizes us and our families. Just like the seeds of a dandelion that grow where they are blown by the wind, we believe in our right and our potential to thrive wherever we land. Just like dandelion seeds that need soil, water and sunlight, we still need basic needs met in order to grow and thrive aquí y allá: safety, health, community, access to educational opportunities, family unification, and legal, international mobility. 
+            In ODA, we seek to be authentic and adventurous.  We are determined to contribute to our community by accepting challenges, as well as letting friendship guide us. We fight the good fight when it comes to Justice for everyone. Kindness, Accountability, Optimism, Community and Love are the values that we share and try to put into action every day.
+            We know, firsthand, about how the current immigration system in the United States criminalizes us and our families. Just like the seeds of a dandelion that grow where they are blown by the wind, we believe in our right and our potential to thrive wherever we land. Just like dandelion seeds that need soil, water and sunlight, we still need basic needs met in order to grow and thrive aquí y allá: safety, health, community, access to educational opportunities, family unification, and legal, international mobility.
           </v-card-text>
           <v-card-text>
             We are committed to the struggle for the full recognition of our Human Rights as immigrants wherever we may find ourselves, and we are also committed to our Human Responsibilities to care for ourselves, our families, our communities, and the natural resources that sustain all of us.
@@ -42,7 +42,9 @@
             <v-card-text>
             Today, ODA is supported by allies around the world, a strong network of returned and deported youth across Mexico, and some of our Leadership Team are: Maggie Loredo, Jill Anderson, Victor Saucedo, Enrique Rojas, Cesar Maldonado, Adriana Figueroa, Victor Hugo Manuel and many more. </v-card-text>
             <v-card-text>
+              <div id="carousel">
                 <about-carousel :people="thePeople"></about-carousel>
+              </div>
             </v-card-text>
         </v-card>
     </v-tab-item>
@@ -83,9 +85,44 @@
     </v-tab-item>
     <v-tab-item>
         <v-card flat>
-            <v-card-text>Contents for Item 2 go here</v-card-text>
+          <v-card-text>
+            <div class="row">
+              <div class="column" id="sixteen">
+                <h2>2016</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+              <div class="column" id="fifteen">
+                <h2>2015</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+              <div class="column" id="fourteen">
+                <h2>2014</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+              <div class="column" id="thirteen">
+                <h2>2013</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+            </div>
+          </v-card-text>
         </v-card>
-    </v-tab-item>  
+    </v-tab-item>
     </v-tabs>
     </v-layout>
     <!-- Spanish version -->
@@ -134,9 +171,44 @@
     </v-tab-item>
     <v-tab-item>
         <v-card flat>
-            <v-card-text>Contents for Item 2 go here</v-card-text>
+          <v-card-text>
+            <div class="row">
+              <div class="column" id="sixteen">
+                <h2>2016</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+              <div class="column" id="fifteen">
+                <h2>2015</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+              <div class="column" id="fourteen">
+                <h2>2014</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+              <div class="column" id="thirteen">
+                <h2>2013</h2>
+                  <ul>
+                    <li>media</li>
+                    <li>media</li>
+                  </ul>
+              </div>
+            </div>
+          </v-card-text>
         </v-card>
-    </v-tab-item>  
+    </v-tab-item>
     </v-tabs>
     </v-layout>
     <site-footer></site-footer>
@@ -174,5 +246,49 @@ export default {
 </script>
 
 <style>
+.column {
+    display: table-cell;
+    float: left;
+    width: 25%;
+    padding: 50px;
+    border-spacing: 1000px;
+}
 
+/* Clear floats after the columns */
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+    width: 100%;
+}
+  #sixteen {
+    margin: 5% auto;
+    width: 20%;
+    border-color: blue;
+    background-color: pink;
+  }
+  #fifteen {
+    margin: 5% auto;
+    width: 20%;
+    border-color: blue;
+    background-color: green;
+  }
+  #fourteen {
+    margin: 5% auto;
+    width: 20%;
+    border-color: blue;
+    background-color: yellow;
+  }
+  #thirteen {
+    margin: 5% auto;
+    width: 20%;
+    border-color: blue;
+    background-color: grey;
+  }
+  #carousel {
+    width: 80%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
 </style>
