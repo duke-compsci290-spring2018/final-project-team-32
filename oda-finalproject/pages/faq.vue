e55bf6389576abd9ca4b69e2baa6fda8deab0175
Charlotte Roh <hr67@duke.edu>
2018-04-28 21:40:23 -0400

    Add files via upload

---
+++ oda-finalproject/pages/faq.vue
@@ -0,0 +1,163 @@
+<template>
+ <v-app id="inspire">
+    <site-header></site-header>
+    <lang-toggle></lang-toggle>
+    <v-card-title class="justify-center"><h1>Frequently Asked Questions</h1></v-card-title>
+    <v-content>
+        <v-container fluid grid-list-xl>
+    <v-layout justify-space-around>
+<!-- <div id="content-container"> -->
+    <ul class="nav-images">
+        <v-flex xs2 class="nav-img">
+            <a href="#" v-scroll-to="'#-LB74ZplvyrrpuZWG0j6'">
+                <img src="https://static1.squarespace.com/static/57374fc227d4bd28d984d633/t/57d753265016e169debd4989/1473729322973/?format=300w" alt="education">
+                <div id="cats">
+                  Education
+                </div>
+            </a>
+        </v-flex>
+        <v-flex xs2 class="nav-img">
+            <a href="#" v-scroll-to="'#-LB74o9dJC3dpk1TW7lp'">
+                <img src="https://static1.squarespace.com/static/57374fc227d4bd28d984d633/t/57cd1771ebbd1afd7c11cbf8/1472582296716/?format=300w" alt="job">
+                <div id="cats">
+                  Jobs
+                </div>
+            </a>
+        </v-flex>
+        <v-flex xs2 class="nav-img">
+            <a href="#" v-scroll-to="'#-LB74vjFL1tl0GDKMprJ'">
+                <img src="https://static1.squarespace.com/static/57374fc227d4bd28d984d633/t/57cd1771ebbd1afd7c11cbf9/1472879828578/bookshelf.png?format=300w" alt="travel">
+                <div id="cats">
+                  Travel
+                </div>
+            </a>
+        </v-flex>
+        <v-flex xs2 class="nav-img">
+            <a href="#" v-scroll-to="'#-LB751f5yvYSUPahtRKF'">
+                <img src="https://static1.squarespace.com/static/57374fc227d4bd28d984d633/t/57cf818203596edd29edb6f9/1473216902429/?format=300w" alt="health">
+                <div id="cats">
+                  Health
+                </div>
+            </a>
+        </v-flex>
+        <v-flex xs2 class="nav-img">
+            <a href="#" v-scroll-to="'#-LB751f5yvYSUPahtRKF'">
+                <img src="https://static1.squarespace.com/static/57374fc227d4bd28d984d633/t/57cd1771ebbd1afd7c11cbfb/1472582368608/?format=300w" alt="document">
+                <div id="cats">
+                  Documentation
+                </div>
+            </a>
+        </v-flex>
+        <v-flex xs2 class="nav-img">
+            <a href="#" v-scroll-to="'#-LB751f5yvYSUPahtRKF'">
+                <img src="https://static1.squarespace.com/static/57374fc227d4bd28d984d633/t/57d73b8b29687fe1896261b9/1473723301538/heart.png?format=300w" alt="citizenship">
+                <div id="cats">
+                  Citizenship
+                </div>
+            </a>
+        </v-flex>
+    </ul>
+    </v-layout>
+                <v-layout v-if="$store.state.role ==='admin'" justify-space-around>
+                <v-form>
+                 <h1>Add New Project</h1>
+                <v-text-field
+                    label="Project Title"
+                    v-model="title"
+                    required
+                ></v-text-field>
+                <v-text-field
+                    label="Project Description"
+                    v-model="description"
+                    required
+                    multi-line
+                    ></v-text-field>
+                <v-text-field
+                    label="Image Link"
+                    v-model="image"
+                    required
+                ></v-text-field>
+                <v-text-field
+                    label="Image Description"
+                    v-model="image_title"
+                    required
+                ></v-text-field>
+                <v-btn v-on:click="addProject" color="info">Add Project</v-btn>
+                </v-form>
+            </v-layout>
+</v-container>
+</v-content>
+</v-app>
+</template>
+
+<script>
+import SiteHeader from '~/components/SiteHeader'
+import ProjectList from '~/components/ProjectList'
+import LangToggle from '~/components/LangToggle'
+import { mapState } from 'vuex'
+import firebase from '~/services/firebaseApp'
+import { projectsRef } from '~/services/firebaseApp'
+import axios from 'axios'
+
+// let pRef = firebase.database().ref('projects')
+export default {
+  name: 'projects',
+  asyncData () {
+    return axios.get('https://oda-finalproject.firebaseio.com/projects.json')
+    .then((res) => {
+      return { projectsTest: res.data }
+    })
+  },
+  components: {
+      SiteHeader,
+      ProjectList,
+      LangToggle
+  },
+  data() {
+    return {
+      title: '',
+      description: '',
+      image: '',
+      image_title: '',
+    };
+  },
+  methods : {
+      addProject: function() {
+        let self = this
+        var myRef = firebase.database().ref().child('projects').push().key
+        firebase.database().ref('projects/' + myRef).set({
+            name: self.title,
+            description: self.description,
+            image_title: self.image,
+            image: self.image,
+            linkName: myRef
+        })
+        this.title='',
+        this.description='',
+        this.image='',
+        this.image_title=''
+      },
+
+  }
+}
+</script>
+
+<style scoped>
+ul{
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+}
+.nav-img img{
+    width: 100px;
+}
+#content-container{
+    margin: 2%;
+}
+#cats {
+  text-align: center;
+}
+
+</style>
